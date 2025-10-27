d1cd58547f218b57ffc1df4c83f6f8cbb56fe786
mjuarez713 <131398645+mjuarez713@users.noreply.github.com>
2025-10-27 19:58:08 -0300

    feat: Aplicar diseño básico con MUI + Tailwind

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 import { store } from './redux'
+import { theme } from './config/theme'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </StrictMode>,
 )
